f03d873241b0b4cce75bd17e0e62f5f85eb27edc
Akira <72250044+Ak1r4Yuk1@users.noreply.github.com>
2020-11-24 00:02:12 +0100

    Version 1.1

---
+++ Windows-Activator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Activator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            MessageBox.Show("First of all disable your internet connection and run this program as Administrator");
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 form = new Form3();
+            form.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string key = "cd '\"c:/Program Files (x86)/Microsoft Office Office16/\" && cscript ospp.vbs /sethst:ug-kms.uni-goettingen.de && cscript ospp.vbs /act";
+            System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            proc.StartInfo = p;
+            proc.Start();
+            proc.WaitForExit();
+            proc.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("chrome.exe", "https://www.youtube.com/ak1r4yuk1");
+            Process.Start("chrome.exe", "https://github.com/Akira96kill");
+        }
+    }
+}
---
+++ Windows-Activator/Form3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Activator
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string key = "slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99";
+            System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            proc.StartInfo = p;
+            proc.Start();
+            proc.WaitForExit();
+            proc.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string key = "slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX";
+            System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            proc.StartInfo = p;
+            proc.Start();
+            proc.WaitForExit();
+            proc.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string key = "slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2";
+            System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            proc.StartInfo = p;
+            proc.Start();
+            proc.WaitForExit();
+            proc.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string key = "slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43";
+            System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            proc.StartInfo = p;
+            proc.Start();
+            proc.WaitForExit();
+            proc.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
