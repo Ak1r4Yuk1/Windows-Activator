d358b4fbeb4b15e84ab69f24a0c6ee1601502fc1
Akira <niko.maiorana@gmail.com>
2020-11-24 00:02:12 +0100

    Version 1.1

---
+++ Windows-Activator/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Windows_Activator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.Location = new System.Drawing.Point(12, 178);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(279, 34);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Info";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Location = new System.Drawing.Point(12, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(279, 34);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Microsoft Office";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Location = new System.Drawing.Point(12, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(279, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Windows 10";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Windows 7";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button6.Location = new System.Drawing.Point(12, 218);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(279, 34);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button6;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(303, 271);
+            this.ControlBox = false;
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Activator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Windows-Activator/Form2.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Activator
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string key = "slmgr /ipk GMJQF-JC7VC-76HMH-M4RKY-V4HX6";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            catch
+            {
+                string key = "slmgr /ipk AXBS6-LR9OV-MEYF5-RMJB9-UCRT2P";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            finally
+            {
+                string key = "slmgr /ipk SF6YR-DEVX1-WSSL4-STPO3-QEX5P";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string key = "slmgr /ipk GMJQF-JC7VC-76HMH-M4RKY-V4HX6";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            catch
+            {
+                string key = "slmgr /ipk AXBS6-LR9OV-MEYF5-RMJB9-UCRT2P";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            finally
+            {
+                string key = "slmgr /ipk SF6YR-DEVX1-WSSL4-STPO3-QEX5P";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string key = "slmgr /ipk 83KKC-GGDK7-39JHV-723GB-XJGFR";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            catch
+            {
+                string key = "slmgr /ipk 7RJ6P-7HM8F-B8HV8-4JDKM-JMRMF";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            finally
+            {
+                string key = "slmgr /ipk BKWPQ-QPCC4-RXH26-BY2DJ-WQHCJ";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string key = "slmgr /ipk 83KKC-GGDK7-39JHV-723GB-XJGFR";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            catch
+            {
+                string key = "slmgr /ipk 7RJ6P-7HM8F-B8HV8-4JDKM-JMRMF";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            finally
+            {
+                string key = "slmgr /ipk BKWPQ-QPCC4-RXH26-BY2DJ-WQHCJ";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string key = "slmgr /ipk 7YWX9-W3C2V-D46GW-P722P-9CP4D";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            catch
+            {
+                string key = "slmgr /ipk MM7DF-G8XWM-J2VRG-4M3C4-GR27X";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            finally
+            {
+                string key = "slmgr /ipk KGMPT-GQ6XF-DM3VM-HW6PR-DX9G8";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string key = "slmgr /ipk 7YWX9-W3C2V-D46GW-P722P-9CP4D";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            catch
+            {
+                string key = "slmgr /ipk MM7DF-G8XWM-J2VRG-4M3C4-GR27X";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+            finally
+            {
+                string key = "slmgr /ipk KGMPT-GQ6XF-DM3VM-HW6PR-DX9G8";
+                System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/C " + key);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo = p;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+            }
+        }
+        
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
